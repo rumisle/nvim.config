74b5883bc140841ecb662e31ac161694a72b02dc
abrasumente <345396594@qq.com>
2023-05-10 22:30:26 +0800

    white mood

--- lua/ab/plugins/lualine.lua
+++ lua/ab/plugins/lualine.lua
@@ -6,13 +6,14 @@ local colors = {
   red    = '#ff5189',
   violet = '#d183e8',
   grey   = '#303030',
+  orange = '#ffbc5d',
   none   = nil,
 }
 
 local bubbles_theme = {
   normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
+    a = { fg = colors.black, bg = colors.orange },
+    b = { fg = colors.black, bg = colors.none },
     c = { fg = colors.violet, bg = colors.none },
   },
 
@@ -21,9 +22,9 @@ local bubbles_theme = {
   replace = { a = { fg = colors.black, bg = colors.red } },
 
   inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
+    a = { fg = colors.white, bg = colors.white },
+    b = { fg = colors.white, bg = colors.white },
+    c = { fg = colors.black, bg = colors.white },
   },
 }
 
@@ -35,14 +36,16 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+      -- { 'mode', separator = { left = '' }, right_padding = 2 },
+      { 'mode', separator = { left = '' }, right_padding = 2 },
     },
     lualine_b = { 'filename', 'branch' },
     lualine_c = { 'fileformat' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
     lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
+      -- { 'location', separator = { right = '' }, left_padding = 2 },
+      { 'location', separator = { right = '' }, left_padding = 2 },
     },
   },
   inactive_sections = {
