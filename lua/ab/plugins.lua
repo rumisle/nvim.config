429474267612d99475f5fc54e35f43bbb90f3cc8
abrasumente <345396594@qq.com>
2024-02-14 18:28:00 +0800

    vim.g.suda_smart_edit must be set before init

--- lua/ab/plugins.lua
+++ lua/ab/plugins.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
 
   {
     "lambdalisue/suda.vim",
-    config = function()
+    init = function()
       -- suda automatically switch a buffer name
       -- when the target file is not readable or writable
       vim.g.suda_smart_edit = 1
