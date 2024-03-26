fb931153a3b3756aff160947a862bfa360a8bf95
abrasumente <345396594@qq.com>
2024-03-27 01:16:07 +0800

    support comment for jsx/tsx, remember `:TSInstall tsx``

--- lua/ab/plugins.lua
+++ lua/ab/plugins.lua
@@ -87,10 +87,21 @@ require("lazy").setup({
 
   -- editing
   {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+    end,
+  },
+  {
     "numToStr/Comment.nvim",
     event = "BufRead",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
     end,
   },
 
