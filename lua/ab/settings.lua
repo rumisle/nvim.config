eeac557ff583b0009b1dfab6cc78768881eed158
abrasumente <why-excuse-quail@duck.com>
2025-01-03 17:48:20 +0800

    use osc 52

--- lua/ab/settings.lua
+++ lua/ab/settings.lua
@@ -56,3 +56,16 @@ vim.filetype.add({
     ll = "llvm",
   },
 })
+
+-- use OSC 52 to copy to system clipboard
+g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
