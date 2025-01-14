87657720cabdfcc3a8033a6cc9d887432291bb28
abrasumente <why-excuse-quail@duck.com>
2025-01-14 20:23:10 +0800

    remove nerd icons

--- lua/ab/plugins/lualine.lua
+++ lua/ab/plugins/lualine.lua
@@ -32,22 +32,23 @@ local bubbles_theme = {
 
 require("lualine").setup({
   options = {
+    icons_enabled = false,
     theme = bubbles_theme,
     component_separators = "|",
-    section_separators = { left = "", right = "" },
+    -- section_separators = { left = "", right = "" },
   },
   sections = {
     lualine_a = {
-      { "mode", separator = { left = "" }, right_padding = 2 },
-      -- { 'mode', separator = { left = '' }, right_padding = 2 },
+      -- { "mode", separator = { left = "" }, right_padding = 2 },
+      { 'mode', separator = { left = '' }, right_padding = 2 },
     },
     lualine_b = { "filename", "branch" },
     lualine_c = { "fileformat" },
     lualine_x = {},
     lualine_y = { "filetype", "progress" },
     lualine_z = {
-      { "location", separator = { right = "" }, left_padding = 2 },
-      -- { 'location', separator = { right = '' }, left_padding = 2 },
+      -- { "location", separator = { right = "" }, left_padding = 2 },
+      { 'location', separator = { right = '' }, left_padding = 2 },
     },
   },
   inactive_sections = {
