98574f05c95b25cdedc02d28cb99e866d64f3ebe
rumisle <why-excuse-quail@duck.com>
2025-11-06 22:28:35 +0800

    support nvim 0.11

--- lua/ab/plugins/lsp/servers.lua
+++ lua/ab/plugins/lsp/servers.lua
@@ -1,20 +1,5 @@
-local lsp = require("lspconfig")
+local lsp = vim.lsp
 local U = require("ab.plugins.lsp.utils")
-
-local flags = {
-  allow_incremental_sync = true,
-  debounce_text_changes = 200,
-}
-
-local capabilities = U.capabilities()
-
-local function on_attach(client, buf)
-  if client.name == "ruff" then
-    -- Disable hover in favor of Pyright
-    client.server_capabilities.hoverProvider = false
-  end
-  U.mappings(client, buf)
-end
 
 -- native diagnostics
 vim.diagnostic.config({
@@ -26,168 +11,25 @@ vim.diagnostic.config({
   },
 })
 
--- language servers
-if U.has_executable("lua-language-server") then
-  lsp.lua_ls.setup({
-    flags = flags,
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-      Lua = {
-        completion = {
-          enable = true,
-          showWord = "Disable",
-        },
-        runtime = {
-          version = "LuaJIT",
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = { os.getenv("VIMRUNTIME") },
-        },
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-  })
-end
+vim.lsp.enable({
+    "lua_ls", -- lua-language-server
+    "rust_analyzer", -- rust_analyzer
+    "clangd", -- clangd
+    "cmake", -- cmake-language-server
+    "ruff", -- ruff
+    "pyright", -- pyright
+    "zls", -- zls
+    "nixd", -- nixd
+    "tsserver", -- tsserver
+    "tailwindcss", -- tailwindcss-language-server
+    "astro", -- astro-ls
+    "mojo", -- mojo-lsp-server
+    "glsl_analyzer", -- glsl_analyzer
+})
 
-if U.has_executable("rust-analyzer") then
-  lsp.rust_analyzer.setup({
-    flags = flags,
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = {
-          allFeatures = true,
-        },
-        checkOnSave = {
-          allFeatures = true,
-          command = "clippy",
-        },
-        procMacro = {
-          ignored = {
-            ["async-trait"] = { "async_trait" },
-            ["napi-derive"] = { "napi" },
-            ["async-recursion"] = { "async_recursion" },
-          },
-        },
-      },
-    },
-  })
-end
 
-if U.has_executable("clangd") then
-  lsp.clangd.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = { "c", "cpp", "cc" },
-    flags = {
-      allow_incremental_sync = true,
-      debounce_text_changes = 500,
-    },
-    cmd = {
-      "clangd",
-      "--offset-encoding=utf-16",
-    },
-  })
-end
+local flags = {
+  allow_incremental_sync = true,
+  debounce_text_changes = 200,
+}
 
-if U.has_executable("cmake-language-server") then
-  lsp.cmake.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("ruff") then
-  lsp.ruff.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    init_options = {
-      settings = {
-        args = {},
-      },
-    },
-  })
-end
-
-if U.has_executable("pyright") then
-  lsp.pyright.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-      pyright = {
-        -- Using Ruff's import organizer
-        disableOrganizeImports = true,
-      },
-      python = {
-        analysis = {
-          -- Ignore all files for analysis to exclusively use Ruff for linting
-          ignore = { "*" },
-        },
-      },
-    },
-  })
-end
-
-if U.has_executable("zls") then
-  lsp.zls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("nixd") then
-  lsp.nixd.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("tsserver") then
-  lsp.tsserver.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("tailwindcss-language-server") then
-  lsp.tailwindcss.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("astro-ls") then
-  lsp.astro.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("mojo-lsp-server") then
-  lsp.mojo.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
-
-if U.has_executable("glsl_analyzer") then
-  lsp.glsl_analyzer.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = flags,
-  })
-end
